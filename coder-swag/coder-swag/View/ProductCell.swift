a469272373e3a6941327466f6b6c3aadf867b361
Ironbatpug <molnarcsaba92@gmail.com>
2019-03-20 20:36:38 +0100

    lesson-04 creating collectionview

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Molnár Csaba on 2019. 03. 20..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+
+}
